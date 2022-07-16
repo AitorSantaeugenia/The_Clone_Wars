5eea57b34c44a3ffc2472b79970268cb886b4e41
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-16 18:13:19 +0200

    added row components and its requests

--- netflix/src/pages/Home.jsx
+++ netflix/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Main from '../components/Main'
+import Row from '../components/Row'
+
+import requests from '../Request'
 
 const Home = () => {
   return (
    <>
    <Main />
+   <Row title="Up Coming" fetchURL={requests.requestUpcoming} />
+   <Row title="Popular" fetchURL={requests.requestPopular} />
+   <Row title="Trending" fetchURL={requests.requestTrending} />
+   <Row title="Top Rated" fetchURL={requests.requestTopRated} />
+   <Row title="Horror" fetchURL={requests.requestHorror} />
    </>
   )
 }
