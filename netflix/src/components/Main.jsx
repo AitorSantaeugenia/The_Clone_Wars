caa57c9c0b9b8638b325086a73a70b1bb2694ae0
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-16 18:12:45 +0200

    added truncate text function

--- netflix/src/components/Main.jsx
+++ netflix/src/components/Main.jsx
@@ -14,6 +14,13 @@ const Main = () => {
     },[])
 
     // console.log(movie)
+    const truncateString = (str, num) => {
+        if(str?.length > num) {
+            return str.slice(0,num) + "..."
+        }else{
+            return str;
+        }
+    }
 
   return (
     <div className="w-full h-[550px] text-white">
@@ -28,7 +35,7 @@ const Main = () => {
                     <button className="border text-white border-gray-300 py-2 px-5 ml-4">Watch Later</button>
                 </div>
                 <p className="text-gray-400 text-sm">Released: {movie?.release_date}</p>
-                <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-w[35%] text-gray-200">{movie?.overview}</p>
+                <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">{truncateString(movie?.overview, 150)}</p>
             </div>
         </div>
     </div>
