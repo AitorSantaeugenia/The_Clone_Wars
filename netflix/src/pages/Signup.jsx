5511c937941c6dabbb74675761b393abc8a6f3da
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-16 20:23:28 +0200

    firebase logout and signup done

--- netflix/src/pages/Signup.jsx
+++ netflix/src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {UserAuth} from "../context/AuthContext"
 
 const Signup = () => {
@@ -7,12 +7,14 @@ const Signup = () => {
   const [password, setPassword] = useState("")
   // eslint-disable-next-line 
   const {user, signUp} = UserAuth()
+  const navigate = useNavigate()
 
   const handleSubmit = async (e) =>{
     e.preventDefault()
 
     try{
       await signUp(email, password)
+      navigate("/")
     }catch(error){
       console.log(error)
     }
@@ -31,8 +33,8 @@ const Signup = () => {
           <div className="max-w-[320px] mx-auto py-16">
             <h1 className="text-3xl font-bold">Sign Up</h1>
             <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
-              <input onChange={(e) => setEmail(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" autocomplete="email" />
-              <input onChange={(e) => setPassword(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" autocomplete="current-password" />
+              <input onChange={(e) => setEmail(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" autoComplete="email" />
+              <input onChange={(e) => setPassword(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" autoComplete="current-password" />
               <button className="bg-red-600 py-3 my-6 rounded font-bold">Sign Up</button>
               <div className="flex justify-between items-center text-sm text-gray-600">
                 <p><input className="mr-2" type="checkbox" />Remember me</p>
