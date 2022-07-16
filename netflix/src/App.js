3790c877158259ee78a5cc156251388a39867176
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-16 20:09:56 +0200

    imported pages and added routes

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>
