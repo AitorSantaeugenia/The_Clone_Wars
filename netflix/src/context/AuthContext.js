16c92a21c439382defdbc7f3b056887370829414
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-17 16:43:34 +0200

    account page created

--- netflix/src/context/AuthContext.js
+++ netflix/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -7,13 +7,18 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
+import { setDoc, doc } from "firebase/firestore";
+
 const AuthContext = createContext();
 
 export function AuthContextProvider({ children }) {
   const [user, setUser] = useState({});
 
   function signUp(email, password) {
-    return createUserWithEmailAndPassword(auth, email, password);
+    createUserWithEmailAndPassword(auth, email, password);
+    setDoc(doc(db, "users", email), {
+      savedShows: [],
+    });
   }
 
   function logIn(email, password) {
--- netflix/src/pages/Login.jsx
+++ netflix/src/pages/Login.jsx
@@ -36,8 +36,8 @@ const Login = () => {
             <h1 className="text-3xl font-bold">Sign In</h1>
             {error ? <p className="p-3 bg-red-400 my-2">{error}</p> : null}
             <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
-              <input onChange={(e) => setEmail(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" autocomplete="email" />
-              <input onChange={(e) => setPassword(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" autocomplete="current-password" />
+              <input onChange={(e) => setEmail(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" autoComplete="email" />
+              <input onChange={(e) => setPassword(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" autoComplete="current-password" />
               <button className="bg-red-600 py-3 my-6 rounded font-bold">Sign In</button>
               <div className="flex justify-between items-center text-sm text-gray-600">
                 <p><input className="mr-2" type="checkbox" />Remember me</p>
