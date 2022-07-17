01364909684456b4142a11be0acd22acf6538914
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-17 23:12:43 +0200

    adde z-index on top to carrousel arrows

--- netflix/src/components/Row.jsx
+++ netflix/src/components/Row.jsx
@@ -26,13 +26,13 @@ const Row = ({title, fetchURL, rowID}) => {
     <>
     <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
     <div className="relative flex items-center group">
-        <MdChevronLeft onClick={slideLeft} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40} />
+        <MdChevronLeft onClick={slideLeft} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-50 hidden group-hover:block" size={40} />
         <div id={"slider" + rowID} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
             {movies.map((item, id) => (
                 <Movie key={id} item={item} />
             ))}
         </div>
-        <MdChevronRight onClick={slideRight} className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40} />
+        <MdChevronRight onClick={slideRight} className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-50 hidden group-hover:block" size={40} />
     </div>
     </>
   )
