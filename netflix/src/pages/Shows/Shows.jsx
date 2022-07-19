8bcce7ca39cd2f06d6a56580a2daa0eeee607fc8
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-19 22:51:48 +0200

    created pages for shows

---
+++ netflix/src/pages/Shows/Shows.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import TVShows from '../../components/ShowHeader/ShowHeader'
+import Row from '../../components/Row'
+
+import requests from '../../Request'
+
+const Shows = () => {
+  return (
+   <>
+   <TVShows />
+   <Row rowID="1" title="Up Coming" fetchURL={requests.requestPopularTV} />
+   <Row rowID="2" title="Popular" fetchURL={requests.requestTopRatedTV} />
+   <Row rowID="3" title="Trending" fetchURL={requests.requestLatestTV} />
+   <Row rowID="4" title="Top Rated" fetchURL={requests.requestPopularTV} />
+   </>
+  )
+}
+
+export default Shows
