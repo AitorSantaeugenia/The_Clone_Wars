480b458a99968e899b5764c20a4a04da7438521e
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-19 22:51:17 +0200

    updated movie.jsx component for both movies and tvshows

--- netflix/src/components/Movie.jsx
+++ netflix/src/components/Movie.jsx
@@ -8,6 +8,13 @@ const Movie = ({item}) => {
     const [like, setLike] = useState([false])
     const [saved, setSaved] = useState(false)
     const {user} = UserAuth()
+    let title = ""
+
+    if(item?.title){
+      title = item?.title;
+    }else{
+      title = item?.name;
+    }
 
     const movieID = doc(db, 'users', `${user?.email}`)
     const saveShow = async () => {
@@ -17,7 +24,7 @@ const Movie = ({item}) => {
         await updateDoc(movieID, {
           savedShows: arrayUnion({
             id: item.id,
-            title: item.title,
+            title: title,
             img: item.backdrop_path
           })
         })
@@ -27,15 +34,19 @@ const Movie = ({item}) => {
     }
 
   return (
-  <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 hover:z-40">
-    <img className="w-full h-auto block" src={`http://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
+    <>
+    {/* Some movies or tvshows have no image (backdrop_path) that's why we do this ternary */}
+    {item?.backdrop_path ? 
+      <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 hover:z-40">
+    <img className="w-full h-auto block" src={`http://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title ? item?.title : item?.name} />
       <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-          <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{item?.title}</p>
+          <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{item?.title ? item?.title : item?.name}</p>
           <p onClick={saveShow}>
               { like ? <FaRegHeart className="absolute top-4 left-4 text-gray-300" /> :  <FaHeart className="absolute top-4 left-4 text-gray-300" />}
           </p>
       </div>
-  </div>
+  </div>: null}
+  </>
   )
 }
 
