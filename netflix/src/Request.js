5036980de4c3d2594dd1b2b98b91ec10766b03b6
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-19 22:50:56 +0200

    updated tvshow request

--- netflix/src/Request.js
+++ netflix/src/Request.js
@@ -7,6 +7,10 @@ const requests = {
   requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
   requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
   requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  //-------------------------------------------------- TV -------------------------------------------------------------------------
+  requestPopularTV: `https://api.themoviedb.org/3/tv/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRatedTV: `https://api.themoviedb.org/3/tv/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestLatestTV: `https://api.themoviedb.org/3/tv/on_the_air?api_key=${key}&language=en-US&page=1`,
 };
 
 export default requests;
