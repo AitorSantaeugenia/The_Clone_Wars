e1aa60610043a8ef2643603e6ffc64394021864c
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-19 22:50:40 +0200

    added tvshows route

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -8,6 +8,7 @@ import Signup from "./pages/Signup";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import LoggedInRoute from "./components/LoggedInRoute";
+import Shows from "./pages/Shows/Shows";
 
 function App() {
   return (
@@ -28,6 +29,14 @@ function App() {
             element={
               <ProtectedRoute>
                 <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/shows"
+            element={
+              <ProtectedRoute>
+                <Shows />
               </ProtectedRoute>
             }
           />
