3c1d9bbb2ece189d6b49c4b03417ac61b908faa4
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-24 22:23:18 +0200

    improved acordion, needs to be better btw

--- netflix/src/pages/LandingPage/LandingPage.jsx
+++ netflix/src/pages/LandingPage/LandingPage.jsx
@@ -4,10 +4,10 @@ import {BsChevronRight, BsGlobe} from 'react-icons/bs'
 import {MdArrowDropDown} from 'react-icons/md'
 
 const LandingPage = () => {
-  let accordion = document.getElementsByClassName("accordion");
-  const totalAccordions = accordion.length;
-
   const buttonAcordeon = () => {
+    let accordion = document.getElementsByClassName("accordion");
+    const totalAccordions = accordion.length;
+    
     for (let i = 0; i < totalAccordions; i++) {
       accordion[i].addEventListener("click", (e) => {
         let panel = e.target.nextElementSibling;
@@ -15,16 +15,20 @@ const LandingPage = () => {
         if (panel.classList[1] === "panel-closed") {
           e.target.classList.toggle("accordion-active");
           panel.classList.toggle("panel-open");
-          for (let i = 0; i < totalAccordions; i++) {
-            let panelToClose = accordion[i].nextElementSibling;
-            if(panelToClose !== panel){
-               accordion[i].classList.remove("accordion-active");
-               panelToClose.classList.remove("panel-open");
-            }
-          }
+          closeAllExcept(panel);
         }
       });
     } 
+    
+    const closeAllExcept = (pan) => {
+      for (let i = 0; i < totalAccordions; i++) {
+        let panelToClose = accordion[i].nextElementSibling;
+        if(panelToClose !== pan){
+           accordion[i].classList.remove("accordion-active");
+           panelToClose.classList.remove("panel-open");
+        }
+      }
+    }
   }
 
   return (
