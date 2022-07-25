543cb802ace6b4138c4d20f99aa6bded65978989
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-25 21:31:22 +0200

    main component changed images styles

--- netflix/src/components/Main.jsx
+++ netflix/src/components/Main.jsx
@@ -27,7 +27,7 @@ const Main = () => {
         <div className="w-full h-full">
             <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
             {/* base url + backdrop_path */}
-            <img className="w-full h-full object-cover" src={`http://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+            <img className="w-full h-full object-cover position-object-image" src={`http://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
             <div className="absolute w-full top-[20%] p-4 md:p-8">
                 <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
                 <div className="my-4">
