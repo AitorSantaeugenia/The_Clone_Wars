30a4314e716e115c4c8bebe78dfb2f1076abe4b5
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-09-14 02:07:50 +0200

    styling movies rows and transformations

--- netflix/src/components/Movies/Movie.jsx
+++ netflix/src/components/Movies/Movie.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
-import { FaHeart, FaRegHeart } from 'react-icons/fa'
+import { FaPlay, FaThumbsUp, FaPlus, FaChevronDown, FaCheck } from 'react-icons/fa'
 import {UserAuth} from '../../context/AuthContext'
 import {db} from '../../firebase'
 import {arrayUnion, doc, updateDoc} from 'firebase/firestore'
+import "./Movie.css"
 
 const Movie = ({item}) => {
     const [like, setLike] = useState([false])
+    // eslint-disable-next-line
     const [saved, setSaved] = useState(false)
+    const [showDiv, setShowDiv] = useState(false)
     const {user} = UserAuth()
     let title = ""
 
@@ -16,8 +19,54 @@ const Movie = ({item}) => {
       title = item?.name;
     }
 
+    // for(let i=0; i>item?.genre_ids.legnth;i++){
+    //   switch (item?.genre_ids[i]) {
+    //     case 28:
+    //       item.genre_ids[i] = "Action";
+    //     case 12:
+    //       return "Adventure";
+    //     case 16:
+    //       return "Animation";
+    //     case 35:
+    //       return "Comedy";
+    //     case 80:
+    //       return "Crime";
+    //     case 99:
+    //       return "Documentary";
+    //     case 18:
+    //       return "Drama";
+    //     case 10751:
+    //       return "Family";
+    //     case 14:
+    //       return "Fantasy";
+    //     case 36:
+    //       return "History";
+    //     case 27:
+    //       return "Horror";
+    //     case 10402:
+    //       return "Music";
+    //     case 9648:
+    //       return "Mystery";
+    //     case 10749:
+    //       return "Romance";
+    //     case 878:
+    //       return "Science Fiction";
+    //     case 10770:
+    //       return "TV Movie";
+    //     case 53:
+    //       item.genre_ids[i] = "Thriller";       
+    //     case 10752:
+    //       return "War";       
+    //     case 37:
+    //       return "Western";  
+                        
+    //   }
+    // }
+   
+
     const movieID = doc(db, 'users', `${user?.email}`)
     const saveShow = async () => {
+      console.log("menta");
       if(user?.email){
         setLike(!like)
         setSaved(true)
@@ -33,19 +82,49 @@ const Movie = ({item}) => {
       }
     }
 
+    const showMovieDiv = () => {
+      setShowDiv(true);
+    }
+
+    const hiddeMovieDiv = () => {
+      setShowDiv(false)
+    }
+
   return (
     <>
     {/* Some movies or tvshows have no image (backdrop_path) that's why we do this ternary */}
     {item?.backdrop_path ? 
-    <div className="2xl:w-[280px] xl:w-[280px] lg:w-[280px] md:w-[240px] sm:w-[200px] smler:w-[200px] inline-block cursor-pointer relative p-2 hover:z-40">
+    <div className="2xl:w-[280px] xl:w-[280px] lg:w-[280px] md:w-[240px] sm:w-[200px] smler:w-[200px] inline-block relative p-2 hover:z-40 fantasy redcolorborder" onClick={showMovieDiv} >
       <img className="w-full h-auto block" src={`http://image.tmdb.org/t/p/w500${item?.backdrop_path}`} alt={item?.title ? item?.title : item?.name} />
-        <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-            <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{item?.title ? item?.title : item?.name}</p>
-            <p onClick={saveShow}>
+        <div className="absolute top-0 left-0 w-full h-full text-white ">
+            <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-[70%] text-center">{item?.title ? item?.title : item?.name}</p>
+            {/* <p onClick={saveShow}>
                 { like ? <FaRegHeart className="absolute top-4 left-4 text-gray-300" /> :  <FaHeart className="absolute top-4 left-4 text-gray-300" />}
-            </p>
+            </p> */}
         </div>
-  </div>: null}
+        <div className={showDiv ? "testHover" : "testHover hidden"}>
+          <div className="flex justify-between items-center">
+            <div className="flex justify-space-around items-center mb-2.5">
+              <div className="movieButtonPlay flex justify-center items-center mr-2.5 cursor-pointer">
+                <FaPlay className="text-[#181818]"/>
+              </div>
+              <div className="movieButtons flex justify-center items-center mr-2.5 cursor-pointer">
+                <FaThumbsUp />
+              </div>
+              <div className="movieButtons flex justify-center items-center mr-2.5 cursor-pointer" onClick={saveShow}>
+                { like ? <FaPlus className=""/> :  <FaCheck />}
+              </div>
+            </div>
+            
+            <div className="movieButtons flex justify-center items-center mb-2.5 cursor-pointer">
+              <FaChevronDown />
+            </div>
+          </div>
+          <div className="mb-2.5">Release date: {item?.release_date}</div>
+          <div>{item?.genre_ids[0] }</div>
+        </div>
+  </div>
+  : null}
   </>
   )
 }
--- netflix/src/components/Row/Row.jsx
+++ netflix/src/components/Row/Row.jsx
@@ -5,6 +5,7 @@ import Movie from '../Movies/Movie'
 
 const Row = ({title, fetchURL, rowID}) => {
     const [movies, setMovies] = useState([])
+    const [sliderLeft, setSliderLeft] = useState(false)
 
     useEffect(() =>{
         axios.get(fetchURL).then((response)=>{
@@ -15,18 +16,25 @@ const Row = ({title, fetchURL, rowID}) => {
    const slideLeft = () =>{
     let slider = document.getElementById('slider' + rowID)
     slider.scrollLeft = slider.scrollLeft - 500;
+
+    if(slider.scrollLeft <= 500){
+        setSliderLeft(false)
+    }
    }
 
    const slideRight = () =>{
     let slider = document.getElementById('slider' + rowID)
     slider.scrollLeft = slider.scrollLeft + 500;
+    setSliderLeft(true);
    }
+
+ 
 
   return (
     <>
     <h2 className="text-white font-bold text-xl md:text-base sm:text-base smler:text-base p-4 2xl:px-16 xl:px-16 lg:px-16 md:px-9 sm:px-9 smler:px-2.5">{title}</h2>
     <div className="relative flex items-center group 2xl:px-16 xl:px-16 lg:px-16 md:px-9 sm:px-9 smler:px-2.5">
-        <MdChevronLeft onClick={slideLeft} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-50 hidden group-hover:block" size={40} />
+        {sliderLeft ? <MdChevronLeft onClick={slideLeft} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-50 hidden group-hover:block" size={40} /> : null}
         <div id={"slider" + rowID} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
             {movies.map((item, id) => (
                 <Movie key={id} item={item} />
