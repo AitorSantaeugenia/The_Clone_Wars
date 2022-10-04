39d45016bc0b84a34426042c964e7f4aa4e5bf18
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-04 04:44:05 +0200

    revert: reverted a change with the size of the div in row

--- netflix/src/components/Row/Row.jsx
+++ netflix/src/components/Row/Row.jsx
@@ -33,7 +33,7 @@ const Row = ({title, fetchURL, rowID}) => {
   return (
     <>
     <h2 className="text-white font-bold text-xl md:text-base sm:text-base smler:text-base p-4 2xl:px-16 xl:px-16 lg:px-16 md:px-9 sm:px-9 smler:px-2.5">{title}</h2>
-    <div className="relative flex items-center group 2xl:px-16 xl:px-16 lg:px-16 md:px-9 sm:px-9 smler:px-2.5 min-h-[325px]">
+    <div className="relative flex items-center group 2xl:px-16 xl:px-16 lg:px-16 md:px-9 sm:px-9 smler:px-2.5">
         {sliderLeft ? <MdChevronLeft onClick={slideLeft} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-50 hidden group-hover:block" size={40} /> : null}
         <div id={"slider" + rowID} className="w-full h-full overflow-x-scroll overflow-y-hidden whitespace-nowrap scroll-smooth scrollbar-hide relative">
             {movies.map((item, id) => (
