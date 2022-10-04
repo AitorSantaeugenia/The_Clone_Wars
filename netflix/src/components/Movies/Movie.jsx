53051cdb0df6ee9649441d4d077968e306222e77
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-04 19:39:15 +0200

    feat: working with a material UI modal when clickin the movies, needs to be responsive

--- netflix/src/components/Movies/Movie.jsx
+++ netflix/src/components/Movies/Movie.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 import { FaPlay, FaThumbsUp, FaPlus, FaChevronDown, FaCheck } from 'react-icons/fa'
+import { IoCloseSharp } from 'react-icons/io5'
 import {UserAuth} from '../../context/AuthContext'
 import {db} from '../../firebase'
 import {arrayUnion, doc, updateDoc} from 'firebase/firestore'
 import "./Movie.css"
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
 
 const Movie = ({item}) => {
     const [like, setLike] = useState([false])
     // eslint-disable-next-line
     const [saved, setSaved] = useState(false)
     const [showDiv, setShowDiv] = useState(false)
+    const [open, setOpen] = useState(false)
     const {user} = UserAuth()
     let title = ""
 
@@ -85,26 +89,62 @@ const Movie = ({item}) => {
 
     const showMovieDiv = () => {
       setShowDiv(true);
+      setOpen(true)
+      console.log(item)
     }
 
     const hiddeMovieDiv = () => {
       setShowDiv(false)
+      setOpen(false)
     }
 
   return (
     <>
     {/* Some movies or tvshows have no image (backdrop_path) that's why we do this ternary */}
     {item?.backdrop_path ? 
-    <div className="2xl:w-[280px] xl:w-[280px] lg:w-[280px] md:w-[240px] sm:w-[200px] smler:w-[200px] inline-block relative p-2 fantasy" onMouseOver={showMovieDiv} onMouseLeave={hiddeMovieDiv}>
+    <div className="2xl:w-[280px] xl:w-[280px] lg:w-[280px] md:w-[240px] sm:w-[200px] smler:w-[200px] inline-block relative p-2 fantasy">
       <div>
         <img className="w-full h-auto block" src={`http://image.tmdb.org/t/p/w500${item?.backdrop_path}`} alt={item?.title ? item?.title : item?.name} />
       </div>
-      <div className="absolute top-0 left-0 w-full h-full text-white hover:opacity-100 hover:bg-black/80">
+      <div className="absolute top-0 left-0 w-full h-full text-white hover:opacity-100 hover:bg-black/80 cursor-pointer">
           <p className={`white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-[100%] text-center`}>{item?.title ? item?.title : item?.name}</p>
           {/* <p onClick={saveShow}>
               { like ? <FaRegHeart className="absolute top-4 left-4 text-gray-300" /> :  <FaHeart className="absolute top-4 left-4 text-gray-300" />}
           </p> */}
       </div>
+      <Button onClick={showMovieDiv}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={hiddeMovieDiv}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className="flex justify-center m-auto items-center"
+      >
+        <div className="max-w-[50%] min-w-[50%] flex flex-col justify-center">
+          <div className="cursor-pointer relative w-full flex justify-end top-[46px] ">
+            <IoCloseSharp className="text-4xl mr-2.5 text-[#fff] bg-[#181818] rounded-[50%] p-1.5 hover:border-2 hover:border-white" onClick={hiddeMovieDiv}/>
+          </div>
+          <div className="max-w-[100%] min-w-[100%]">
+            <img className="w-full block" src={`http://image.tmdb.org/t/p/w500${item?.backdrop_path}`} alt={item?.title ? item?.title : item?.name} />
+          </div>
+
+          <div className="flex absolute px-12 py-4 mt-36">
+            <div className="flex justify-center items-center mr-2.5 cursor-pointer">
+              <button className="border bg-slate-100 text-black border-gray-300 hover:bg-gray-300 py-2 px-5 font-bold flex items-center min-w-[110px]"><FaPlay className="mr-3 "/>Play</button>
+            </div>
+            <div className="movieButtons flex justify-center items-center mr-2.5 cursor-pointer" onClick={saveShow}>
+              { like ? <FaPlus className=""/> :  <FaCheck />}
+            </div>
+            <div className="movieButtons flex justify-center items-center mr-2.5 cursor-pointer">
+                <FaThumbsUp />
+            </div>
+          </div>
+          <div className="bg-[#141414] px-12 py-4">
+            <p className="text-gray-400 text-sm pb-4">Released: {item?.release_date}</p>
+            <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">{item?.overview}</p>
+          </div>
+        </div>
+      </Modal>
       {/* <div className={showDiv ? "testHover block" : "testHover hidden"}>
         <div className="flex justify-between items-center">
           <div className="flex justify-space-around items-center mb-2.5">
