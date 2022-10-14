d03aa22f7856e65523f60cd98f47ac5e57614b3e
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-14 20:57:23 +0200

    refractor: refractoring code in genres component

--- netflix/src/components/Genres/Genres.jsx
+++ netflix/src/components/Genres/Genres.jsx
@@ -1,26 +1,22 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Genres = ({genre}) => {
-  // console.log(genre)
-
   const[genres, setGenres] = useState([])
-
 
   useEffect(() =>{
     const id = genre.id;
     const key = process.env.REACT_APP_IMDB_API_KEY; 
 
     axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}`).then((response) =>{
-     console.log(response.data.genres)
      setGenres(response.data.genres)
     })
-  },[])
+  },[genre.id])
 
   return (
     <div className="flex">
       {genres.map((item) => (
-          <span>
+          <span key={item.name}>
             <span className="mx-2">·</span> {item.name}
           </span>
        ))}
