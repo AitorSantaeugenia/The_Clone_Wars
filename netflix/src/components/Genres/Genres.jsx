07893bd61ccdd27b2a8ad91a88f768747cb69a0b
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-16 02:10:23 +0200

    fix: fixed an issue with shows category, now shows properly shows and movies categories

--- netflix/src/components/Genres/Genres.jsx
+++ netflix/src/components/Genres/Genres.jsx
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Genres = ({genre}) => {
+const Genres = ({genre, type}) => {
   const[genres, setGenres] = useState([])
 
   useEffect(() =>{
     const id = genre.id;
     const key = process.env.REACT_APP_IMDB_API_KEY; 
 
-    axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}`).then((response) =>{
-     setGenres(response.data.genres)
-    })
+    if(type === "movie"){
+      axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}`).then((response) =>{
+        setGenres(response.data.genres)
+       })
+    }else{
+      axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${key}`).then((response) =>{
+        setGenres(response.data.genres)
+       })
+    }
   },[genre.id])
 
   return (
-    <div className="flex">
+    <div className="flex flex-wrap">
       {genres.map((item) => (
-          <span key={item.name}>
+          <span key={item.name} className="text-sm">
             <span className="mx-2">·</span> {item.name}
           </span>
        ))}
