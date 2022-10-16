fb98d503c50b8ea84258ea945a8d0a888a0595e3
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-16 02:12:37 +0200

    feat: added type to know if its a movie or a show

--- netflix/src/pages/Home.jsx
+++ netflix/src/pages/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
   return (
    <>
    <Main />
-   <Row rowID="1" title="Up Coming" fetchURL={requests.requestUpcoming} />
-   <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
-   <Row rowID="3" title="Trending" fetchURL={requests.requestTrending} />
-   <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
-   <Row rowID="5" title="Horror" fetchURL={requests.requestHorror} />
+   <Row rowID="1" title="Up Coming" fetchURL={requests.requestUpcoming} type={"movie"}/>
+   <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} type={"movie"}/>
+   <Row rowID="3" title="Trending" fetchURL={requests.requestTrending} type={"movie"}/>
+   <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} type={"movie"}/>
+   <Row rowID="5" title="Horror" fetchURL={requests.requestHorror} type={"movie"}/>
    </>
   )
 }
