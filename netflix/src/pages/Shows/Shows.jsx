64c9037485d28abe0e92a30892a7ac5fb8f36980
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-16 02:13:35 +0200

    fix: added a type to know that it's a show

--- netflix/src/pages/Shows/Shows.jsx
+++ netflix/src/pages/Shows/Shows.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import TVShows from '../../components/ShowHeader/ShowHeader'
 import Row from '../../components/Row/Row'
-
 import requests from '../../Request'
 
 const Shows = () => {
   return (
    <>
    <TVShows />
-   <Row rowID="1" title="Up Coming" fetchURL={requests.requestPopularTV} />
-   <Row rowID="2" title="Popular" fetchURL={requests.requestTopRatedTV} />
-   <Row rowID="3" title="Trending" fetchURL={requests.requestLatestTV} />
-   <Row rowID="4" title="Top Rated" fetchURL={requests.requestPopularTV} />
+   <Row rowID="1" title="Up Coming" fetchURL={requests.requestPopularTV} type={"show"}/>
+   <Row rowID="2" title="Popular" fetchURL={requests.requestTopRatedTV} type={"show"}/>
+   <Row rowID="3" title="Trending" fetchURL={requests.requestLatestTV} type={"show"}/>
+   <Row rowID="4" title="Top Rated" fetchURL={requests.requestPopularTV} type={"show"}/>
    </>
   )
 }
