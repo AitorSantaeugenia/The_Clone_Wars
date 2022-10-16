23146e19179cd8dc1637ddfbf1c1a9ae9399d507
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-16 02:11:52 +0200

    fix: added type to know if it's a movie or a tvshow

--- netflix/src/components/Row/Row.jsx
+++ netflix/src/components/Row/Row.jsx
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from 'react'
 import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 import Movie from '../Movies/Movie'
 
-const Row = ({title, fetchURL, rowID}) => {
+const Row = ({title, fetchURL, rowID, type}) => {
     const [movies, setMovies] = useState([])
     const [sliderLeft, setSliderLeft] = useState(false)
+    const [typeShow] = useState(type)
 
     useEffect(() =>{
         axios.get(fetchURL).then((response)=>{
@@ -37,7 +38,7 @@ const Row = ({title, fetchURL, rowID}) => {
         {sliderLeft ? <MdChevronLeft onClick={slideLeft} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-50 hidden group-hover:block" size={40} /> : null}
         <div id={"slider" + rowID} className="w-full h-full overflow-x-scroll overflow-y-hidden whitespace-nowrap scroll-smooth scrollbar-hide relative">
             {movies.map((item, id) => (
-                <Movie key={id} item={item} />
+                <Movie key={id} item={item} type={typeShow}/>
             ))}
         </div>
         <MdChevronRight onClick={slideRight} className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-50 hidden group-hover:block" size={40} />
