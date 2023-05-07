3ff62b5c964a9281996c2505025a80c9c9cd718d
AitorSantaeugenia <aitorsantaeugenia@gmail.com>
2023-05-07 20:17:01 +0200

    feat: updated showheader modal button

--- netflix/src/components/ShowHeader/ShowHeader.jsx
+++ netflix/src/components/ShowHeader/ShowHeader.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
-import requests from '../../Request'
+import requests from '../../../src/Request'
 import "./ShowHeader.css"
 import { FaPlay } from 'react-icons/fa';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
